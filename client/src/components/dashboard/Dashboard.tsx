6573aa65dddb6f7b5dcc994ebc494122c1254d78
SilvenLEAF <SilvenLEAF@gmail.com>
2021-03-20 10:50:06 +0530

    DEMO APP

---
+++ client/src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,35 @@
+import M from 'materialize-css'
+
+
+import React, { useEffect, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+
+import NamePlate from './NamePlate';
+ 
+ 
+
+import { AuthContext } from '../../contexts/subContexts/AuthContext';
+
+
+
+
+function Dashboard() {
+  useEffect(()=>{
+    M.AutoInit();    
+  }, [])
+
+  
+  const { userData } = useContext(AuthContext);
+  const history = useHistory();
+
+  if(!userData) history.push('/login');
+  if(userData && !userData.isVerified) history.push('/verifyDoor');
+  
+  return (
+    <>      
+      <NamePlate/>
+    </>
+  )
+}
+
+export default Dashboard
